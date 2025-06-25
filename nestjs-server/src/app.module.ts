78bc9a98b6dec712efdb883876f259b929daa5b4
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-06-25 22:39:05 +0400

    add config util

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { IS_DEV_ENV } from '@/libs/common/utils'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		})
+	],
 	controllers: [],
 	providers: []
 })
---
+++ nestjs-server/src/libs/common/utils/is-dev.util.ts
@@ -0,0 +1,11 @@
+import { ConfigService } from '@nestjs/config'
+import * as dotenv from 'dotenv'
+import * as process from 'node:process'
+
+dotenv.config()
+
+export const isDevUtil = (configService: ConfigService) => {
+	return configService.getOrThrow('NODE_ENV') === 'development'
+}
+
+export const IS_DEV_ENV = process.env.NODE_ENV === 'development'
