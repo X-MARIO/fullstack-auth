41a20181ee429766e5b03847f0dcdedf82e39f00
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-06-30 22:38:36 +0400

    WiP

--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -1,15 +1,17 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import {ConflictException, Injectable, InternalServerErrorException} from '@nestjs/common'
+import {Request} from 'express'
 
-import { RegisterDto } from '@/auth/dto'
-import { UserService } from '@/user/user.service'
+import {RegisterDto} from '@/auth/dto'
+import {UserService} from '@/user/user.service'
 
-import { AuthMethod } from '../../generated/prisma'
+import {AuthMethod, User} from '../../generated/prisma'
 
 @Injectable()
 export class AuthService {
 	public constructor(private readonly userService: UserService) {}
 
-	public async register(dto: RegisterDto) {
+	public async register(req: Request, dto: RegisterDto) {
+		console.log('>> dto', dto);
 		const isExists = await this.userService.findBEmail(dto.email)
 
 		if (isExists) {
@@ -27,9 +29,31 @@ export class AuthService {
 			false
 		)
 
-		return newUser
+		return newUser;
 	}
 	public async login() {}
+
 	public async logout() {}
-	public async saveSession() {}
+
+	public async saveSession(req: Request, user: User) {
+		console.log('>> user', user);
+		return new Promise((resolve, reject) => {
+			console.log('>> user', user);
+			req.session.userId = user.id
+
+			req.session.save(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии.'
+						)
+					)
+				}
+
+				resolve({
+					user
+				})
+			})
+		})
+	}
 }
