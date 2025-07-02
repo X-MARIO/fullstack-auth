ba897d010f6acd5d427ea16c5e943135f0321a55
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-07-02 10:18:46 +0400

    add getProvidersConfig

--- nestjs-server/src/auth/provider/provider.service.ts
+++ nestjs-server/src/auth/provider/provider.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
+
+import { ProviderOptionsSymbol, TypeOptions } from './provider.constants'
+import { BaseOAuthService } from './services/base-oauth.service'
 
 @Injectable()
-export class ProviderService {}
+export class ProviderService implements OnModuleInit {
+	public constructor(
+		@Inject(ProviderOptionsSymbol) private readonly options: TypeOptions
+	) {}
+
+	public onModuleInit(): void {
+		for (const provider of this.options.services) {
+			provider.baseUrl = this.options.baseUrl
+		}
+	}
+
+	public findByService(service: string): BaseOAuthService | null {
+		return this.options.services.find(s => s.name === service) ?? null
+	}
+}
