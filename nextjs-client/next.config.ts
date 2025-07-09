320a17391b0ebbdc8f06fc6eb7476bf9154ad0de
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-07-09 21:21:41 +0400

    init shadcn ui

--- nextjs-client/next.config.ts
+++ nextjs-client/next.config.ts
@@ -3,6 +3,21 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'standalone',
+    env: {
+        SERVER_URL: process.env.SERVER_URL,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.yandex.net'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
