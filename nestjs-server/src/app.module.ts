e3c3dcaf37dc61832e04fec4ec962d00d49f2804
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-07-09 14:25:33 +0400

    add recovery

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { EmailConfirmationModule } from '@/auth/email-confirmation/email-confirmation.module'
+import { PasswordRecoveryModule } from '@/auth/password-recovery/password-recovery.module'
 import { ProviderModule } from '@/auth/provider/provider.module'
 import { IS_DEV_ENV } from '@/libs/common/utils'
 import { MailModule } from '@/libs/mail/mail.module'
@@ -21,7 +22,8 @@ import { UserModule } from './user/user.module'
 		AuthModule,
 		ProviderModule,
 		MailModule,
-		EmailConfirmationModule
+		EmailConfirmationModule,
+		PasswordRecoveryModule
 	],
 	controllers: [],
 	providers: []
--- nestjs-server/src/libs/mail/mail.service.ts
+++ nestjs-server/src/libs/mail/mail.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { render } from '@react-email/components'
 
+import { ResetPasswordTemplate } from '@/libs/mail/templates/reset-password.template'
+
 import { ConfirmationTemplate } from './templates/confirmation.template'
 
 @Injectable()
@@ -17,6 +19,13 @@ export class MailService {
 		const html = await render(ConfirmationTemplate({ domain, token }))
 
 		return this.sendMail(email, 'Подтверждение почты', html)
+	}
+
+	public async sendPasswordResetEmail(email: string, token: string) {
+		const domain = this.configService.getOrThrow<string>('ALLOWED_ORIGIN')
+		const html = await render(ResetPasswordTemplate({ domain, token }))
+
+		return this.sendMail(email, 'Сброс пароля', html)
 	}
 
 	private sendMail(email: string, subject: string, html: string) {
