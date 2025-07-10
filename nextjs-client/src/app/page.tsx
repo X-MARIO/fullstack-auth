42b08b37d488d4b060417a163b3bf0941457a1c7
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-07-10 16:06:58 +0400

    add button variants

--- nextjs-client/src/app/page.tsx
+++ nextjs-client/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import {buttonVariants} from "@/shared/components/ui";
 
 export default function Home() {
   return (
     <div className='space-y-5 text-center'>
       <h1 className='text-4xl font-bold'>Главная страница</h1>
-      <Link href='/auth/login/' />
+        <Link href='/auth/login/' className={buttonVariants()}>
+            Войти в аккаунт
+        </Link>
     </div>
   );
 }
